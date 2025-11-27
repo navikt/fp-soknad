d2f62cc37645e7297097ffc309ebe67ca2b0e984
Michal J. Sladek <michal.sladek@nav.no>
2025-11-27 09:05:28 +0100

    chore: fjerner ubrukt annotering.

--- src/main/java/no/nav/foreldrepenger/soknad/server/ServiceStarterListener.java
+++ src/main/java/no/nav/foreldrepenger/soknad/server/ServiceStarterListener.java
@@ -3,7 +3,6 @@ package no.nav.foreldrepenger.soknad.server;
 import jakarta.enterprise.inject.spi.CDI;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
-import jakarta.servlet.annotation.WebListener;
 
 /**
  * Starter interne applikasjontjenester
